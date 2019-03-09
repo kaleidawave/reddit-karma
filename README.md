1ec8147ce041b207f152e86c863c19d8c1729624
kaleidagraph <26967284+kaleidagraph@users.noreply.github.com>
2019-03-09 11:03:18 +0000

    Imporoved results, added readme, changed spin animation

---
+++ README.md
@@ -0,0 +1,5 @@
+# Real Reddit Karma
+
+This website provides a more accurate version of a users reddit karma. 
+
+Icons from [FontAwesome](https://fontawesome.com/) 
--- index.js
+++ index.js
@@ -3,24 +3,32 @@ function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+function userToUrl(user) {
+    return `https://www.reddit.com/user/${user}`;
+}
+
+function urlToUser(url) {
+    return url.substring(url.lastIndexOf('/user/') + 5, url.length).replace('/', '');
+}
+
 async function getKarma(event) {
     event.preventDefault();
-    document.getElementById('score').innerHTML = '<i class="fas fa-spinner spin"></i> Loading';
+    document.getElementById('score').innerHTML = '<i class="fas fa-spinner fa-spin"></i> Loading';
     let input = document.getElementById('username').value;
-    let username = input.includes('www.reddit.com/user/') ? input.substring(input.lastIndexOf('/user/') + 5, input.length).replace('/', '') : input;
+    let username = input.includes('www.reddit.com/user/') ? urlToUser(input) : input;
     const response = await fetch(`https://www.reddit.com/user/${username}.json?sort=top&limit=100`);
     if (response.ok) {
         const json = await response.json();
         let scores = json.data.children.map(post => post.data.score);
         if (scores.length === 0) {
-            document.getElementById('score').innerHTML = `User: ${username} does not have any posts or comments`;
+            document.getElementById('score').innerHTML = `User: <a href="${userToUrl(username)}">${username}</a> does not have any posts or comments`;
         }
         else {
             let score = scores.reduce((a, b) => a + b);
-            document.getElementById('score').innerHTML = `User: ${username} has ${numberWithCommas((score))} total upvotes`;
+            document.getElementById('score').innerHTML = `User: <a href="${userToUrl(username)}">${username}</a> has ${numberWithCommas((score))} total upvotes`;
         }
     }
     else {
-        document.getElementById('score').innerHTML = `User: ${username} does not exist at https://www.reddit.com/user/${username}`;
+        document.getElementById('score').innerHTML = `User: <a href="${userToUrl(username)}">${username}</a> does not exist`;
     }
 }
